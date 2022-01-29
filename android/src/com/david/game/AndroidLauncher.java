5372bb6bf02fa050bd340124b5c0daf3bd0f9e1e
SightMan3 <Lukasran28@gmail.com>
2022-01-29 11:02:15 +0100

    d

---
+++ android/src/com/david/game/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.david.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.david.game.DavidRenderer;
+
+import android.util.Log;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DavidRenderer(), config);
+	}
+}
